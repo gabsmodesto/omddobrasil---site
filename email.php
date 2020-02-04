bdcc4126890263f7b7d6ce39c0c102a661b5d4d7
gabsmodesto <58002091+gabsmodesto@users.noreply.github.com>
2020-02-04 17:23:02 -0300

    Update email.php

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST(['email']) && !empty($_POST(['email'])))){
+if(isset($_POST(['email']) && !empty($_POST(['email'])){
 
 $nome = addcslashes ($_POST (['name']))
 $email = addcslashes ($_POST (['email']))
