eb44ee5df154870b2f994aa49f64eada8edeb6b0
gabsmodesto <58002091+gabsmodesto@users.noreply.github.com>
2020-02-05 10:22:23 -0300

    Update email.php

--- email.php
+++ email.php
@@ -1,29 +1,59 @@
 <?php
 
-if ((!empty($nome)) && (!empty($email)) && (!empty($assunto)) && (!empty($mensagem))) {
+if (isset($_POST['btnSend'])) {
 
-$nome = addcslashes ($_POST (['name']))
-$email = addcslashes ($_POST (['email']))
-$assunto = addcslashes ($_POST (['assunto']))
-$mensagem = addcslashes ($_POST (['message']))
+    $msg = '';
+    
+    //campos do formulario
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $assunto = $_POST["assunto"];
+    $mensagem = $_POST['mensagem'];
 
-$to = "almoxarifado@omddobrasil.com.br"
-$subject = "Contato - OMD do Brasil"
-$body = "Nome: ".$nome. "\r\n"
-        "Email: ".$email. "\r\n"
-        "Assunto: ".$assunto. "\r\n"
-        "Mensagem: ".$mensagem;
-$header = "From: almoxarifado@servitel.com.br"."\r\n"
-    ."Reply-To:".$email."\e\n"
-    ."X=Mailer:PHP/".phpversion ();
+    //valida se os campos não estão vazios
+    if ((!empty($nome)) && (!empty($email)) && (!empty($assunto)) && (!empty($mensagem))) {
+        
+        $email_remetente = "almoxarifado@omddobrasil.com.br"; // deve ser uma conta de email do seu dominio 
+        $email_destinatario = $email; // email que receberá as mensagens
+        $email_recebidoDe = "$email";
+        $email_assunto = "Envio Teste do Formulário de Contato"; // Este será o assunto da mensagem
+        $email_conteudo = "FORMULÁRIO DE CONTATO\n"
+            . "<br><b>De:</b> " . $nome
+            . "<br><b>Email:</b> " . $email
+            . "<br><b>Telefone:</b> " . $telefone
+            . "<br><b>Assunto:</b> " . $assunto
+            . "<br><b>Mensagem:</b> " . $mensagem
+            . "<br><br>"
+            . "<hr>"
+            . "<br>Mensagem enviada do formulário de contato da demonstração de formulário de contato com php.";
 
-if(mail($to,$subject,$body,$header)){
+        //encapsula os dados do cabeçalho do email
+        $email_cabecalho = implode("\n", array("From: $email_remetente", "Reply-To: $email_recebidoDe", "Return-Path: $email_remetente", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
+        
+        //utiliza função nativa do php mail para o envio
+        //valida se o email foi enviado
+        if (mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_cabecalho)) {
+            
+            //mostra mensagem de envio com sucesso
+            $msg = '<div class="alert alert-success alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Mensagem enviada com sucesso!</strong> 
+            </div>';
+        } else {
 
-    echo("E-mail enviado com sucesso, em breve retornaremos!");
-}else{
-    echo("O e-mail não pode ser enviado.") ";"
+            //mostra mensagem de erro ao enviar
+            $msg = '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Erro ao enviar mensagem, tente novamente! </strong> 
+            </div>';
+        }
+        
+    } else {
+        
+        //mostra mensagem de erro caso algum dos campos esteja vazio
+        $msg = '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Preencha todos os campos!! </strong> 
+            </div>';
+    }
 }
-
-}
-
-?>
